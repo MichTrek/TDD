175a7e558b6794cf826afb84a697a08839a64952
franiczio <michal.fr@nelak.pl>
2019-03-13 13:49:19 +0100

    check is returns true

--- src/test/java/PalindromeTest.java
+++ src/test/java/PalindromeTest.java
@@ -1,5 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromeTest {
-
+    Palindrome palindrome = new Palindrome();
+    @Test
+    public void testIfCheckPalindromeReturnsTrue(){
+        assertTrue(palindrome.checkPalindrome());
+        
+    }
 }
